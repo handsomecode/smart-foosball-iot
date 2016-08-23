11a0e0ee65c5e417572cbad697cdcd8851ddf0c9
Eugene Komarov <eugenek@handsome.is>
2016-08-23 19:44:45 +0600

    android add firebase service class

---
+++ AndroidApp/app/src/main/java/is/handsome/labs/iotfoosball/ActionListener.java
@@ -0,0 +1,25 @@
+package is.handsome.labs.iotfoosball;
+
+import java.util.List;
+
+public class ActionListener <T> {
+    protected void addingPerformed(String key, T data, int index) {
+
+    }
+
+    protected void removingPerformed(String key, T data, int index) {
+
+    }
+
+    protected void changingPerformed(String key, T data, int index) {
+
+    }
+
+    protected void initialisation(List<String> keyList, List<T> dataList) {
+
+    }
+
+    protected void listenerRemovingPerformed() {
+
+    }
+}
---
+++ AndroidApp/app/src/main/java/is/handsome/labs/iotfoosball/PlayerWithScoreList.java
@@ -0,0 +1,12 @@
+package is.handsome.labs.iotfoosball;
+
+public class PlayerWithScoreList {
+
+
+    private class playerWithScore {
+        public String playerId;
+        public Player player;
+        public int wins;
+        public int losses;
+    }
+}
